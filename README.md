4c78da82c165af6897a53114f0bc9c50a10eec73
Naseefabu <naseefbloomberg@gmail.com>
2023-04-19 22:06:02 +0530

    misc

--- README.md
+++ README.md
@@ -4,5 +4,48 @@
 
 
 # instructions
+
+
+
+
+# misc
+
 download apache : https://kafka.apache.org/
 sudo apt update
+
+Every broker on a Kafka cluster is also named a Bootstrap Server. All brokers have the metadata required for the clients (producer or consumer) to discover brokers. When a client connects to one of the brokers (which are already configured as bootstrap servers in the Kafka configuration) it makes a "metadata request". The response includes information about topics, partitions, leader brokers, etc. Once the client gets this info, then - in the case of a producer- it makes the write request directly to the leader broker for that specific partition.
+
+
+
+Topics:
+
+Topic : binance-trade
+Partitions : symbols
+
+Topic : binance-orderbook
+Partitions : symbols
+
+Topic : binance-delta
+Partitions : symbols
+
+Topic : coinbase-trade
+Partitions : symbols
+
+Topic : coinbase-orderbook
+Partitions : symbols
+
+Topic : coinbase-delta
+Partitions : symbols
+
+Topic : kraken-trade
+Partitions : symbols
+
+Topic : kraken-orderbook
+Partitions : symbols
+
+Topic : kraken-delta
+Partitions : symbols
+
+
+
+Run the consumer first 
---
+++ python/binance_producer.py
@@ -0,0 +1,82 @@
+from binance.websocket.spot.websocket_client import SpotWebsocketClient
+from binance.spot import Spot as Client
+from kafka.admin import KafkaAdminClient, NewTopic
+from functools import partial
+from kafka import KafkaProducer
+import multiprocessing
+import pandas as pd
+import logging
+import json
+import time
+import os
+
+
+def json_serializer(data):
+    return json.dumps(data).encode("utf-8")
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],value_serializer=json_serializer)
+
+base_url = 'https://api.binance.com'
+stream_url = 'wss://stream.binance.com:9443/ws'
+client = Client(base_url=base_url)
+ws_client = SpotWebsocketClient(stream_url=stream_url)
+order_book = {
+    "lastUpdateId": 0,
+    "bids": [],
+    "asks": []
+}
+
+
+def orderbook_message_handler(symbol,message): 
+    
+    print(symbol,symbols_dict[symbol])
+
+    # producer.send()
+
+
+def listen_binance_orderbook(symbol):
+    ws_client.start()
+    symbol = symbol.lower()
+    ws_client.diff_book_depth(
+        symbol=symbol.lower(),
+        id=1,
+        speed=1000,
+        callback=lambda message: orderbook_message_handler(symbol, message)
+    )
+
+def load_symbols():
+    with open('binance_config.json') as f:
+        data = json.load(f)
+    symbol_dict = {symbol: number for symbol, number in data.items()}
+    return symbol_dict
+
+symbols_dict = load_symbols()
+
+def main():
+
+    global symbols_dict
+    try:
+        admin = KafkaAdminClient(bootstrap_servers='localhost:9092')
+
+        topic = NewTopic(name='binance-orderbook',
+                            num_partitions=len(symbols_dict),
+                            replication_factor=1)
+        admin.create_topics([topic])
+    except Exception:
+        pass
+    
+    processes = []
+    for key, value in symbols_dict.items():
+        p = multiprocessing.Process(target=listen_binance_orderbook, args=(key,))
+        processes.append(p)
+
+    # start all processes
+    for p in processes:
+        p.start()
+
+    # wait for all processes to complete
+    for p in processes:
+        p.join()
+
+
+main()
