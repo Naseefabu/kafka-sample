50437edaad114005326d0d613855b2da3ceb42c3
Naseefabu <naseefbloomberg@gmail.com>
2023-04-20 12:23:44 +0530

    multiprocessing

--- python/strategy_consumer.py
+++ python/strategy_consumer.py
@@ -22,19 +22,3 @@ while True:
         print("binance orderbook = {}".format(json.loads(msg.value)))
 
 
-
-#################
-
-# consumer = KafkaConsumer(
-#     'my_topic',                    # topic name
-#     bootstrap_servers=['localhost:9092'], # Kafka broker(s) connection string
-#     group_id='my_group',           # consumer group ID
-#     auto_offset_reset='earliest',  # earliest offset available
-#     enable_auto_commit=True,       # commit offsets automatically
-#     value_deserializer=lambda x: json.loads(x.decode('utf-8')) # deserialize message value as JSON
-# )
-
-# # consume events
-# for message in consumer:
-#     event = message.value
-#     print(f"Received {event['event_type']} event for user {event['user_id']} at {event['timestamp']}")
