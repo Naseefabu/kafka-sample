c98bea05c31bd87f60eca469f11d40ac707241d4
Naseefabu <naseefbloomberg@gmail.com>
2023-04-21 22:55:54 +0530

    utils

--- python/utils.py
+++ python/utils.py
@@ -12,7 +12,7 @@ def get_orderbook_datafeed(exchange, symbol):
             raise RuntimeError("Symbol in binance exchange is not found. Please input valid symbol")
         consumer = KafkaConsumer(bootstrap_servers=['localhost:9092'],
                             group_id='strategy-marketmaking-binance',
-                            auto_offset_reset='earliest'
+                            auto_offset_reset='latest'
                             )
         topic = "binance"+"-orderbook"
         partition = TopicPartition(topic, symbol_dict[symbol])
@@ -27,7 +27,7 @@ def get_orderbook_datafeed(exchange, symbol):
             raise RuntimeError("Symbol in coinbase exchange is not found. Please input valid symbol")
         consumer = KafkaConsumer(bootstrap_servers=['localhost:9092'],
                             group_id='strategy-marketmaking-coinbase', 
-                            auto_offset_reset='earliest'
+                            auto_offset_reset='latest'
                             )
         topic = "coinbase"+"-orderbook"
         partition = TopicPartition(topic, symbol_dict[symbol])
@@ -41,9 +41,37 @@ def get_orderbook_datafeed(exchange, symbol):
             raise RuntimeError("Symbol in kraken exchange is not found. Please input valid symbol")
         consumer = KafkaConsumer(bootstrap_servers=['localhost:9092'],
                             group_id='strategy-marketmaking-kraken', # fix ?
-                            auto_offset_reset='earliest'
+                            auto_offset_reset='latest'
                             )
         topic = "kraken"+"-orderbook"
+        partition = TopicPartition(topic, symbol_dict[symbol])
+        consumer.assign([partition])
+        return consumer
+    elif (exchange == "bitmex"):
+        with open('bitmex_config.json') as f:
+            data = json.load(f)
+        symbol_dict = {symbol: number for symbol, number in data.items()}
+        if symbol not in symbol_dict.keys():
+            raise RuntimeError("Symbol in bitmex exchange is not found. Please input valid symbol")
+        consumer = KafkaConsumer(bootstrap_servers=['localhost:9092'],
+                            group_id='strategy-marketmaking-bitmex', # fix ?
+                            auto_offset_reset='latest'
+                            )
+        topic = "bitmex"+"-orderbook"
+        partition = TopicPartition(topic, symbol_dict[symbol])
+        consumer.assign([partition])
+        return consumer
+    elif (exchange == "bybit"):
+        with open('bybit_config.json') as f:
+            data = json.load(f)
+        symbol_dict = {symbol: number for symbol, number in data.items()}
+        if symbol not in symbol_dict.keys():
+            raise RuntimeError("Symbol in bybit exchange is not found. Please input valid symbol")
+        consumer = KafkaConsumer(bootstrap_servers=['localhost:9092'],
+                            group_id='strategy-marketmaking-bybit', # fix ?
+                            auto_offset_reset='latest'
+                            )
+        topic = "bybit"+"-orderbook"
         partition = TopicPartition(topic, symbol_dict[symbol])
         consumer.assign([partition])
         return consumer
