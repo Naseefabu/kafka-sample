7a51c08095045cf009b38c7afc7d356ab1ace380
Naseefabu <naseefbloomberg@gmail.com>
2023-04-24 10:53:49 +0530

    misc

---
+++ cpp/src/producers/binance_producer.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+#include <boost/beast.hpp>
+#include <boost/beast/ssl.hpp>
+
+int main(){
+    std::cout << "hello world " << std::endl;    
+}
---
+++ cpp/src/producers/build/CMakeFiles/binance.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/binance.dir/binance_producer.cpp.o"
+  "binance"
+  "binance.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/binance.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
